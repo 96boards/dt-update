5ab9b3454ee25fbc68ba46b3edf54ccdf29e2d78
Loic Poulain <loic.poulain@linaro.org>
2018-04-20 11:40:34 +0200

    db410c: Add MAXIM DS1307 overlay/script
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

---
+++ scripts/db410c/enable-ds1307-i2c0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BOOTIMG=${1:-/dev/disk/by-partlabel/boot}
+DT_ROOT=`dirname $0`/../..
+DBOOTIMG=`which dbootimg`
+DBOOTIMG=${DBOOTIMG:-${DT_ROOT}/tools/dbootimg/dbootimg}
+DTBTOOL=`which dtbtool`
+DTBTOOL=${DTBTOOL:-${DT_ROOT}/tools/dtbtool/dtbtool}
+OVERLAY=`dirname $0`/overlays/db410c-ds1307-i2c0.dtbo
+
+${DBOOTIMG} ${BOOTIMG} -x dtb | ${DTBTOOL} -m ${OVERLAY} | ${DBOOTIMG} ${BOOTIMG} -u dtb
+
+if [ "$?" -ne 0 ]; then
+	echo "Failed to update DTB"
+	exit 1
+fi
+
+echo "Maxim DS1307 enabled, please reboot"
+sync
